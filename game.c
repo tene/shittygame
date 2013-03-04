ca727c258d4719d244972ee988f5ea28bf718d1f
Stephen Weeks <tene@allalone.org>
2013-03-03 17:14:32 -0800

    Draw a ship

--- game.c
+++ game.c
@@ -12,7 +12,7 @@ int
 main(void)
 {
    Ecore_Evas *ee;
-   Evas_Object *bg;
+   Evas_Object *bg, *ship;
 
    ecore_evas_init();
 
@@ -29,9 +29,30 @@ main(void)
    if (bg == ecore_evas_object_associate_get(ee))
      printf("Association worked!\n");
 
-   evas_object_event_callback_add(bg, EVAS_CALLBACK_MOUSE_MOVE, _mouse_move, NULL);
+   //evas_object_event_callback_add(bg, EVAS_CALLBACK_MOUSE_MOVE, _mouse_move, NULL);
 
    ecore_evas_cursor_set(ee, "cursor.png", 0, 10, 10);
+
+   ship = evas_object_image_filled_add(ecore_evas_get(ee));
+   evas_object_image_file_set(ship, "ship.png", NULL);
+   evas_object_resize(ship, 50, 50);
+   evas_object_move(ship, 90, 90);
+   evas_object_show(ship);
+
+   int x, y, w, h, img_w, img_h;
+   evas_object_geometry_get(ship, &x, &y, &w, &h);
+   evas_object_image_size_get(ship, &img_w, &img_h);
+   printf("geom: %d %d %d %d %d %d\n", x, y, w, h, img_w, img_h);
+   Evas_Map *map = evas_map_new(4);
+   evas_map_util_points_populate_from_object(map, ship);
+   evas_map_point_image_uv_set(map, 0, 0, 0);
+   evas_map_point_image_uv_set(map, 1, img_w, 0);
+   evas_map_point_image_uv_set(map, 2, img_w, img_h);
+   evas_map_point_image_uv_set(map, 3, 0, img_h);
+   evas_map_util_rotate(map, 45, x + (w/2), y + (h/2));
+   evas_object_map_set(ship, map);
+   evas_object_map_enable_set(ship, EINA_TRUE);
+   evas_map_free(map);
 
    ecore_main_loop_begin();
 
